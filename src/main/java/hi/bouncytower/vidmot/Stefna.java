d14c20b0d91946118da53b116a3efa098a5163fc
Stulli888 <sfm1@hi.is>
2023-04-12 13:36:11 +0000

    Smá refactor og setti listener fyrir orvatakka

---
+++ src/main/java/hi/bouncytower/vidmot/Stefna.java
@@ -0,0 +1,22 @@
+package hi.bouncytower.vidmot;
+
+public enum Stefna {
+    VINSTRI(180),
+    HAEGRI(0),
+    UPP(90),
+    NIDUR(270);
+
+    private final int stefna;
+
+    /**
+     * Smiður fyrir stefnu hlut
+     * @param stefna heiltala fenginn frá KeyCode sem passar við mögulegar stefnur
+     */
+    Stefna(int stefna){this.stefna = stefna;}
+
+    /**
+     * Getter fyrir stefnu
+     * @return heiltölu gildi stefnunnar
+     */
+    public int getStefna(){return stefna;}
+}
